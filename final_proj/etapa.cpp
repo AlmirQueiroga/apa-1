10aac4b7d26b5eda9ecec2b4daecc543f77de124
Lucas Fernandes Lucena <lucas.ferlucena@gmail.com>
2018-10-24 17:52:32 -0300

    Add files via upload

--- final_proj/etapa.cpp
+++ final_proj/etapa.cpp
@@ -4,55 +4,55 @@
 #include <string.h>
 #include <fstream>
 #include <vector>
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 
 # define INF std::numeric_limits<int>::max()
 
 using namespace std;
 
-int prim(vector<pair<int , int>> adj[], int raiz, int N){
-    int chave[N];
-    int pai[N];
-    for(int i = 0; i < N; i++){
-        chave[i] = INF;
-        pai[i] = -1;
+vector<int> resultado;
+
+//verifica se um dado vertice esta contido na atual soluçao
+int estaContido(int a){
+    for(int i = 0; i < resultado.size(); i++){
+        if(a == resultado[i])
+            return 1;
     }
-    
-    std::vector<bool> inMST(N, false);
+    return 0;
+}
 
-    priority_queue< pair<int, int>, vector <pair<int, int>> , greater<pair<int, int>> > pq;
-    pq.push(make_pair(0, raiz)); 
-
-    chave[raiz] = 0;
-
-    while(!pq.empty()){
-        int u = pq.top().second; 
-        pq.pop(); 
-
-        inMST[u] = true;
-
-        for(int i = 0; i < adj[u].size(); i++){
-            int v = adj[u][i].first;
-            int w = adj[u][i].second;
-
-            if(inMST[v] == false && w < chave[v]){
-                pai[v] = u;
-                chave[v] = w;  
-                pq.push(make_pair(chave[v], v));
-                cout << chave[v] << endl << endl;
+//Metodo do vizinho mais proximo
+void construir(std::vector<pair<int, int>> p[]){
+    int vertice = 0;
+    resultado.push_back(0);
+    while(1){
+        sort(p[vertice].begin(), p[vertice].begin() + 29);
+        for(int i = 0; i < 29; i++){
+            if(p[vertice][i].first != 0 && !estaContido(p[vertice][i].second)){
+                resultado.push_back(p[vertice][i].second);
+                vertice = p[vertice][i].second;
+                break;
             }
         }
+        if(resultado.size() == 29)
+            break;
+    }
+}
+
+//Calcula o valor do caminho presente no ciclo presente em resultados
+int calcularValorCaminho(std::vector<pair<int, int>> p[]){
+    int soma = 0;
+
+    for(int i = 0, j = 1; j < resultado.size(); i++, j++){
+        soma += p[resultado[i]][resultado[i]].first;
     }
 
-    int soma = 0;
-    for(int i = 0; i < N; i++){
-        soma += chave[i];  
-        cout<<soma<<endl;
-    }
+    soma += p[resultado[0]][resultado[28]].first;
+
     return soma;
 }
 
-
+//leitura do arquivo e chamada de funçoes
 int main(){
     //leitura do arquivo
     FILE *fp;
@@ -62,7 +62,7 @@ int main(){
         puts("arquivo nao encontrado");
         exit(1);
     }
-    
+
     int qLines = 0;
     int size;
     int tamanho;
@@ -81,34 +81,24 @@ int main(){
 
         if (feof(fp))
             break;
-        
+
         int index = 0;
 
         token = strtok(line, " ");
-
+        //cout << qLines << endl;
         while(token!=NULL){
             p[qLines].push_back(make_pair(atoi(token), index));
-            
+            //cout << index << "# :" << atoi(token) << " ";
             token = strtok(NULL, " ");
             index++;
         }
         qLines++;
     }
+
     fclose(fp);//fim da leitura
-/*
-    int minimumCost;
-    int raiz = 0;
 
-    minimumCost = prim(adj, raiz, size);
+    construir(p);
+    calcularValorCaminho(p);
 
-    cout << minimumCost << endl;
-  */  
-
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){ 
-            cout << p[i][j].second << " ";
-        }       
-        cout << endl;
-    }
     return 0;
 }
